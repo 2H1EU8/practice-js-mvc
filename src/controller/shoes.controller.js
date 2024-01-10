de570d4538f5c4b3609f90ce73ae56e9febacf15
manmandh <mantm.22ite@vku.udn.vn>
2024-01-10 22:55:33 +0700

    function add

--- src/controller/shoes.controller.js
+++ src/controller/shoes.controller.js
@@ -1,23 +1,8 @@
 import Shoes from "../model/shoes.model";
 
 class ShoesController{
-
-    async addShoes(shoeData) {
-        try {
-
-            if (!shoeData) {
-                throw new Error('Invalid shoes data provided');
-            }
-
-            const newShoe = new Shoes(shoeData);
-            this.Shoes.push(newShoe);
-
-            return newShoe;
-
-        } catch (error) {
-            console.error('Error', error);
-            throw error; 
-        }
+    constructor(shoesView, shoesService){
+        
     }
 }
 
