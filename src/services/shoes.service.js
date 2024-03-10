f47c4ba4fcf416a86e72c03a062e71ef28dac93d
manmandh <mantm.22ite@vku.udn.vn>
2024-03-10 09:30:51 +0700

    ref: respesponsive

--- src/services/shoes.service.js
+++ src/services/shoes.service.js
@@ -7,14 +7,6 @@ class ShoesService{
         this.shoes = []
     }
 
-    async changePassword(firstName,lastName, password, email, id){
-        try{
-            config.patch(`/users/${id}`, {firstName, lastName, password, email})
-        }catch(err){
-            createToast('error', 'Error fetching password again');
-        }
-    }
-
     async updateStatus(id, status){
         try {
             await config.patch(`/shoes/${id}`, {status});
